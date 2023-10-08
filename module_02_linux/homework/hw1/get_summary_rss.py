7d3172109741129c3e79fe19ca1f097625819f06
anosov miron <mairon26rus@gmail.com>
2023-10-08 21:49:37 +0200

    completed get_summary_rss.py

--- module_02_linux/homework/hw1/get_summary_rss.py
+++ module_02_linux/homework/hw1/get_summary_rss.py
@@ -12,13 +12,38 @@ $ ps aux > output_file.txt
 а возвращает суммарный объём потребляемой памяти в человекочитаемом формате.
 Это означает, что ответ надо перевести в байты, килобайты, мегабайты и так далее.
 """
+import os.path
+import subprocess
 
 
 def get_summary_rss(ps_output_file_path: str) -> str:
-    ...
+    """
+    Функция создает файл и извлекает из него данные из столбца RSS для форматирования данных и возврата в виде str.
+
+    Arg:
+        ps_output_file_path: str: Путь к файлу.
+    Returns:
+        str: RAM mb
+    """
+    rss: int = 0
+    index_rss: int | None = None
+    try:
+        with open(ps_output_file_path, 'w', encoding='utf-8') as file_create:
+            subprocess.run(['ps', 'aux'], stdout=file_create, text=True)
+            with open(ps_output_file_path, 'r', encoding='utf-8') as file_read:
+                for line in file_read:
+                    bite_date = line.split()
+                    if index_rss is None:
+                        index_rss = bite_date.index('RSS')
+                    if bite_date[index_rss].isdigit():
+                        rss += int(bite_date[index_rss])
+            rss = int(rss / (1024 * 2))
+        return f'{rss} Mb'
+    except (FileNotFoundError, IOError) as er:
+        print(er)
 
 
 if __name__ == '__main__':
-    path: str = 'PATH_TO_OUTPUT_FILE'
+    path: str = os.path.abspath(os.path.join('output_file.txt'))
     summary_rss: str = get_summary_rss(path)
     print(summary_rss)
