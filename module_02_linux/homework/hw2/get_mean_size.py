f36710d26ba929c2085b60bb2da3593ac5aef62a
anosov miron <mairon26rus@gmail.com>
2023-10-09 16:04:27 +0200

    completed get_mean_size.py

--- module_02_linux/homework/hw2/get_mean_size.py
+++ module_02_linux/homework/hw2/get_mean_size.py
@@ -10,11 +10,24 @@ $ ls -l | python3 get_mean_size.py
 import sys
 
 
-def get_mean_size(ls_output: str) -> float:
-    ...
+def get_mean_size(ls_output: list) -> float:
+    count_bite: float = 0.0
+    count_file: int = 0
+    with open('ls.txt', 'w', encoding='utf-8') as file:
+        for line in ls_output:
+            file.write(f'{line}')
+    try:
+        for line in ls_output:
+            float_dig: float = float(line.split()[4])
+            count_bite += float_dig
+            count_file += 1
+        count_bite = count_bite / count_file
+        return count_bite
+    except ZeroDivisionError:
+        return 0.0
 
 
 if __name__ == '__main__':
-    data: str = sys.stdin.read()
+    data: list = sys.stdin.readlines()[1:]
     mean_size: float = get_mean_size(data)
     print(mean_size)
