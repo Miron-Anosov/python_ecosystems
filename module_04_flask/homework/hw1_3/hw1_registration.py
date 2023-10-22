bf8d1c7a025be0c22ae90850504af9f3984e6b96
anosov miron <mairon26rus@gmail.com>
2023-10-22 11:26:51 +0200

    added validators for RegistrationForm

--- module_04_flask/homework/hw1_3/hw1_registration.py
+++ module_04_flask/homework/hw1_3/hw1_registration.py
@@ -12,17 +12,30 @@
 from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField
+from wtforms.validators import InputRequired, Email, Length
+
 from hw2_validators import number_length, NumberLength
 
 app = Flask(__name__)
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField()
-    phone = IntegerField()
-    name = StringField()
-    address = StringField()
-    index = IntegerField()
+    """Базовый класс для обработки пост запросов при регистрации."""
+    email = StringField(
+        validators=[InputRequired(message="The email can't be empty"), Email(message="The email doesn't correct")])
+
+    phone = IntegerField(
+        validators=[InputRequired(message="The phone can't be empty"),
+                    number_length(min_value=1, max_value=999_999_99_99, message='The phone is invalid'),
+                    NumberLength(min_len=10, max_len=10, message='The phone must be length only 10 numbers')])
+
+    name = StringField(validators=[InputRequired(message="The name can't be empty")])
+
+    address = StringField(validators=[InputRequired(message="The address can't be empty"),
+                                      Length(min=3, max=100, message='The address is too short')])
+
+    index = IntegerField(validators=[InputRequired(message="The index can't be empty"),
+                                     NumberLength(min_len=5, max_len=6, message='The index length invalid')])
     comment = StringField()
 
 
