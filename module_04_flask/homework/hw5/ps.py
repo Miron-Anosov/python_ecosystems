bf0c31c111c1f8b821df70bd866692b44661f106
anosov miron <mairon26rus@gmail.com>
2023-10-23 17:21:32 +0200

    import html

--- module_04_flask/homework/hw5/ps.py
+++ module_04_flask/homework/hw5/ps.py
@@ -10,6 +10,7 @@
 import subprocess
 import shlex
 from typing import List
+import html
 
 from flask import Flask, request
 
@@ -23,11 +24,11 @@ def ps() -> str:
     command_str = " ".join(shlex.quote(str_) for str_ in args)
     command = shlex.split(command_str)
     command.insert(0, 'ps')
+    print(command)
     result = subprocess.run(command, capture_output=True, text=True).stdout
-    return f"<pre>{result}</pre>"
-    # TODO для корректного отображения текста в формате html надо заменять символы <, >, и ":
-    #  https://ruseller.com/lessons.php?rub=1&id=2106 иначе текст может отображаться не так как вы ожидаете,
-    #  например часть текста отбрасывается или зачеркиваться т.д.
+    ps_text = html.escape(result)
+    ps_html = f"<pre>{ps_text}</pre>"
+    return ps_html
 
 
 if __name__ == "__main__":
