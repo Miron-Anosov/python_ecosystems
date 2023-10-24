7fd77f54416992e477a0ab8180a03e3c97b6fde2
anosov miron <mairon26rus@gmail.com>
2023-10-24 23:47:21 +0200

    completed app.py

--- module_05_processes_and_threads/homework/hw1/app.py
+++ module_05_processes_and_threads/homework/hw1/app.py
@@ -10,6 +10,7 @@
 которая на вход принимает порт и запускает по нему сервер. Если порт будет занят,
 она должна найти процесс по этому порту, завершить его и попытаться запустить сервер ещё раз.
 """
+import subprocess
 from typing import List
 
 from flask import Flask
@@ -27,7 +28,20 @@ def get_pids(port: int) -> List[int]:
         raise ValueError
 
     pids: List[int] = []
-    ...
+    command: str = f'lsof -i :{port}'
+    with subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True) as procs:
+        procs.wait()
+        if procs.returncode != 1:
+            ports_line: List[str] = procs.stdout.read().strip().split('\n')  # разбиваем на строки.
+            top_line_output = ports_line[0].split()  # извлекаем шапку таблицы.
+            index_pid: int = top_line_output.index('PID')  # извлекаем индекс.
+            for i_process in range(1, len(ports_line)):  # обход строк со второй строки.
+                try:  # извлекаем pid и преобразуем строковый тип данных в целочисленный
+                    pid: int = int(ports_line[i_process].split()[index_pid])
+                    pids.append(pid)
+                except TypeError:
+                    raise TypeError(f'{pid} is not integer')
+
     return pids
 
 
@@ -36,8 +50,18 @@ def free_port(port: int) -> None:
     Завершает процессы, занимающие переданный порт
     @param port: порт
     """
+    command = 'kill'
     pids: List[int] = get_pids(port)
-    ...
+    if pids:
+        list_process = []
+        for i_procs in pids:
+            process = subprocess.Popen([command, str(i_procs)])
+            list_process.append(process)
+        for proc in list_process:
+            try:
+                proc.wait()
+            except subprocess.TimeoutExpired:
+                subprocess.run(f'kill -9 {proc}', capture_output=True)
 
 
 def run(port: int) -> None:
