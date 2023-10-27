037b2ee1a80295e2d7e6eb36c4c8dfed30e9519b
anosov miron <mairon26rus@gmail.com>
2023-10-27 18:21:57 +0200

    completed self_printing.py

--- module_05_processes_and_threads/homework/hw5_add/self_printing.py
+++ module_05_processes_and_threads/homework/hw5_add/self_printing.py
@@ -2,9 +2,17 @@
 Напишите код, который выводит сам себя.
 Обратите внимание, что скрипт может быть расположен в любом месте.
 """
+import os
+import sys
+
+path_dir = os.path.dirname(__file__)
+path_full = os.path.join(path_dir, __file__)
 
 result = 0
 for n in range(1, 11):
     result += n ** 2
 
-# Secret magic code
+with open(path_full, 'r', encoding='utf-8') as pyfile:
+    code = pyfile.read()
+    sys.stdout.write(code)
+
