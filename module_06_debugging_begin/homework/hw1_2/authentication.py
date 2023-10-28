c3b1d5235a8ecd805254f74bd62f43a0d95a3122
anosov miron <mairon26rus@gmail.com>
2023-10-28 22:14:02 +0200

    completed authentication.py

--- module_06_debugging_begin/homework/hw1_2/authentication.py
+++ module_06_debugging_begin/homework/hw1_2/authentication.py
@@ -17,18 +17,36 @@
 
 import getpass
 import hashlib
-import logging
+import logging.config
+import os.path
+import re
+from logging import Formatter, FileHandler, StreamHandler
+from typing import List
+import requests
 
 logger = logging.getLogger("password_checker")
 
+url = 'https://gist.githubusercontent.com/wchargin/8927565/raw/d9783627c731268fb2935a731a618aa8e95cf465/words'
+
+with requests.get(url) as page:
+    text: List[str] = page.text.split()
+    words: set = set(i for i in text if len(i) > 4)
+
 
 def is_strong_password(password: str) -> bool:
-    return True
+    if len(password) < 5:
+        return True
+    pattern: str = r'\b\w+\b'
+    list_pattern: List[str] = re.findall(pattern, password)
+    for pass_key in list_pattern:
+        if pass_key in words:
+            return True
+    return False
 
 
 def input_and_check_password() -> bool:
     logger.debug("Начало input_and_check_password")
-    password: str = getpass.getpass()
+    password: str = getpass.getpass().lower()
 
     if not password:
         logger.warning("Вы ввели пустой пароль.")
@@ -51,8 +69,27 @@ def input_and_check_password() -> bool:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    file: str = os.path.abspath(os.path.join('stderr.txt'))
+    # формат сообщений для файлов
+    forma_file: Formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s ', datefmt='%H:%M:%S')
+    # формат сообщений для терминала
+    forma_terminal: Formatter = logging.Formatter(fmt='%(message)s')
+
+    logger.setLevel(level=logging.DEBUG)
+    # поток в файл
+    file_handler: FileHandler = logging.FileHandler(filename=file, mode='w', encoding='utf-8')
+    file_handler.setFormatter(fmt=forma_file)
+    file_handler.setLevel(level=logging.DEBUG)
+    # поток в терминал
+    terminal_handler: StreamHandler = logging.StreamHandler()
+    terminal_handler.setFormatter(fmt=forma_terminal)
+    terminal_handler.setLevel(level=logging.INFO)
+    # потоки передаются в handler
+    logger.addHandler(hdlr=file_handler)
+    logger.addHandler(hdlr=terminal_handler)
+
     logger.info("Вы пытаетесь аутентифицироваться в Skillbox")
+
     count_number: int = 3
     logger.info(f"У вас есть {count_number} попыток")
 
