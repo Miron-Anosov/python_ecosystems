4fe1d39ff42a1f211b6e57f3d6a84d3f884f7842
anosov miron <mairon26rus@gmail.com>
2023-10-30 17:21:10 +0100

    completed main.py

--- module_06_debugging_begin/homework/hw4/main.py
+++ module_06_debugging_begin/homework/hw4/main.py
@@ -8,7 +8,37 @@
 4. Сколько сообщений содержит слово dog.
 5. Какое слово чаще всего встречалось в сообщениях уровня WARNING.
 """
-from typing import Dict
+import json
+import os.path
+import subprocess
+import shlex
+from typing import Dict, List
+from collections import defaultdict
+
+
+def read_log_file() -> List[dict]:
+    """
+    Функция извлекает данные из лог-файла в формате json.
+    Returns:
+         Список состоит из словарей формата json, которые в свою очередь являются логами.
+         List[dict]
+    """
+    json_list: List[dict] = []
+    skillbox_json_messages_file: str = os.path.abspath(os.path.join('skillbox_json_messages.log'))
+    try:
+
+        with open(skillbox_json_messages_file, 'r', ) as log_file:
+            for skillbox_log_messages in log_file:
+                json_file: dict = json.loads(skillbox_log_messages)
+                json_list.append(json_file)
+
+    except FileNotFoundError:
+        return []
+    return json_list
+
+
+json_objects: List[dict] = read_log_file()
+file = os.path.abspath(os.path.join('skillbox_json_messages.log'))
 
 
 def task1() -> Dict[str, int]:
@@ -16,7 +46,10 @@ def task1() -> Dict[str, int]:
     1. Сколько было сообщений каждого уровня за сутки.
     @return: словарь вида {уровень: количество}
     """
-    pass
+    call_logs: Dict[str, int] = defaultdict(int)
+    for log in json_objects:
+        call_logs[log.get('level')] += 1
+    return dict(call_logs)
 
 
 def task2() -> int:
@@ -24,7 +57,14 @@ def task2() -> int:
     2. В какой час было больше всего логов.
     @return: час
     """
-    pass
+    try:
+        call_logs = defaultdict(int)
+        for log in json_objects:
+            call_logs[log.get('time')[:2]] += 1
+        max_call_in_hour: int = sorted(call_logs.items(), key=lambda x: x[1]).pop()[0]
+        return max_call_in_hour
+    except IndexError:
+        return 0
 
 
 def task3() -> int:
@@ -32,7 +72,12 @@ def task3() -> int:
     3. Сколько логов уровня CRITICAL было в период с 05:00:00 по 05:20:00.
     @return: количество логов
     """
-    pass
+
+    cmd_str = f'grep -c \\"time\\":\\ \\"05:[0-1][0-9]:[0-5][0-9]\\",\\ \\""level\\": \\"CRITICAL\\"" {shlex.quote(file)}'
+    cmd_list: List[str] = shlex.split(cmd_str)
+    with subprocess.Popen(cmd_list, stdout=subprocess.PIPE, text=True, encoding='utf-8') as result:
+        count_call = int(result.stdout.read())
+    return count_call
 
 
 def task4() -> int:
@@ -40,7 +85,14 @@ def task4() -> int:
     4. Сколько сообщений содержат слово dog.
     @return: количество сообщений
     """
-    pass
+    try:
+        cmd_str: str = f"""grep -ciw "dog" {shlex.quote(file)}"""
+        cmd_list: List[str] = shlex.split(cmd_str)
+        with subprocess.Popen(cmd_list, stdout=subprocess.PIPE, text=True, encoding='utf-8') as result:
+            count_dog: int = int(result.stdout.read())
+        return count_dog
+    except IndexError:
+        return 0
 
 
 def task5() -> str:
@@ -48,7 +100,16 @@ def task5() -> str:
     5. Какое слово чаще всего встречалось в сообщениях уровня WARNING.
     @return: слово
     """
-    pass
+    try:
+        include_in_warn = defaultdict(int)
+        for log in json_objects:
+            if log.get("level") == "WARNING":
+                for message in log.get('message').split():
+                    include_in_warn[message.lower()] += 1
+        word: str = sorted(include_in_warn.items(), key=lambda x: x[1]).pop()[0]
+        return word
+    except IndexError:
+        return 'not found word'
 
 
 if __name__ == '__main__':
