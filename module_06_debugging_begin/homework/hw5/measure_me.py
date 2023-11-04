084fbf3db41b6cf0a38d52837614c5e60eb4855e
anosov miron <mairon26rus@gmail.com>
2023-11-04 13:38:25 +0100

    completed measure_me.py

--- module_06_debugging_begin/homework/hw5/measure_me.py
+++ module_06_debugging_begin/homework/hw5/measure_me.py
@@ -6,8 +6,10 @@
 в начале и в конце которой пишется "Enter measure_me" и "Leave measure_me".
 Сконфигурируйте логгер, запустите программу, соберите логи и посчитайте среднее время выполнения функции measure_me.
 """
+import json
 import logging
 import random
+from datetime import datetime, timedelta
 from typing import List
 
 logger = logging.getLogger(__name__)
@@ -60,7 +62,41 @@ def measure_me(nums: List[int]) -> List[List[int]]:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level="DEBUG")
+    logging.basicConfig(
+        filename=f'{__name__}.log',
+        filemode='w',
+        encoding='utf-8',
+        level="DEBUG",
+        format='{"time" : "%(asctime)s,%(msecs)d", "message" : "%(message)s"}',
+        datefmt='%H:%M:%S',
+
+    )
+
     for it in range(15):
         data_line = get_data_line(10 ** 3)
         measure_me(data_line)
+
+    list_worked_func: list = []
+    log_time_for_one_cycle: list = []
+    with open(f'{__name__}.log', 'r', ) as log_files:
+        for time_logs in log_files:
+            log: dict = json.loads(time_logs)
+
+            if log.get("message") == "Enter measure_me":
+                time_start: str = log.get("time")
+                microsecond_start: int = datetime.strptime(time_start, "%H:%M:%S,%f").microsecond
+                log_time_for_one_cycle.append(microsecond_start)
+
+            if log.get("message") == "Leave measure_me":
+                time_finish: str = log.get("time")
+                microsecond_finish: int = datetime.strptime(time_finish, "%H:%M:%S,%f").microsecond
+                log_time_for_one_cycle.append(microsecond_finish)
+
+            if len(log_time_for_one_cycle) == 2:
+                time_func: int = abs(log_time_for_one_cycle.pop() - log_time_for_one_cycle.pop())
+                list_worked_func.append(time_func)
+
+    if list_worked_func:
+        time_func_around: float = sum(list_worked_func) / len(list_worked_func)
+        second_run: timedelta = timedelta(microseconds=time_func_around)
+        print(f'Среднее затраченное время работы функции measure_me():  {second_run}')
