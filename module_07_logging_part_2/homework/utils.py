98818ed470fff8731c35330ac3abfe02b04fe293
Miron  Anosov <mairon26rus@gmail.com>
2023-11-10 13:55:32 +0100

    added utils.py

--- module_07_logging_part_2/homework/utils.py
+++ module_07_logging_part_2/homework/utils.py
@@ -1,6 +1,9 @@
+import logging
+
 from typing import Union, Callable
 from operator import sub, mul, truediv, add
 
+logger = logging.getLogger('app.utils')
 
 OPERATORS = {
     '+': add,
@@ -17,12 +20,20 @@ def string_to_operator(value: str) -> Callable[[Numeric, Numeric], Numeric]:
     Convert string to arithmetic function
     :param value: basic arithmetic function
     """
+
+    logger.info('Start string_to_operator, ---test ascii True---')
+    logger.info(f' Value is validate: {value}')
+    logger.info('Value check as str')
     if not isinstance(value, str):
-        print("wrong operator type", value)
-        raise ValueError("wrong operator type")
+        logger.error(f'Wrong operator type, type not must be {type(value)}, {value=}')
+        raise ValueError("Wrong operator type, it's not str")
+    logger.debug('Value is string')
 
     if value not in OPERATORS:
-        print("wrong operator value", value)
-        raise ValueError("wrong operator value")
+        logger.error(f'Wrong operator type, {value}, arithmetic function must be: ( + - * / )')
+        raise ValueError("Wrong operator value, it's must be basic arithmetic function")
 
+    logger.debug('Value is in OPERATORS')
+    logger.info(f'Value is returns : {value}')
+    logger.info('Сlose string_to_operator, ---test ascii False---')
     return OPERATORS[value]
