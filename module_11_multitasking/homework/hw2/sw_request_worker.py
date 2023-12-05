64107f17bec09c6b6842a07f78595b9ea57592f1
Miron <mairon26rus@gmail.com>
2023-12-05 11:39:38 +0100

    fix ather_threads

--- module_11_multitasking/homework/hw2/sw_request_worker.py
+++ module_11_multitasking/homework/hw2/sw_request_worker.py
@@ -1,7 +1,7 @@
 import logging
 import threading
 import time
-from typing import Callable, List
+from typing import Callable
 from functools import wraps
 
 from request_func import star_wars_request
@@ -9,12 +9,20 @@ from db_manager import sqlite_manager as db
 
 logger: logging = logging.getLogger(__name__)
 
-__all__ = ['major_thread', 'twenty_threads']
+__all__ = ['major_thread', 'ather_threads']
 
 
 def timer(func: Callable) -> Callable:
+    """
+    Декоратор-таймер. Обрабатывает затраченное время на выполнение энных операций.
+    Args:
+        func: Callable
+    Returns:
+        func: Callable
+    """
+
     @wraps(func)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs) -> Callable:
         start_request: float = time.time()
         result = func(*args, **kwargs)
         finish: float = time.time() - start_request
@@ -24,44 +32,28 @@ def timer(func: Callable) -> Callable:
     return wrapper
 
 
-def sqlite3_db(data_characters: List[tuple[str, str, str]], name_db: str) -> None:
-    """
-    Функция записывает данные об персонажах в базу данных.
-    Args:
-        name_db: str: Название БД.
-        data_characters:List:
-            name: Имя персонажа.
-            age: Возраст персонажа.
-            gender: Пол персонажа.
-            name_db: Название БД.
-
-    Returns:
-        None
-    """
-    with db(name_db=name_db) as base:
-        for name, age, gender in data_characters:
-            base.insert_new_actor(name=name, age=age, gender=gender)
-
-
-def response_threads(data_db: List, queue_characters: int) -> None:
+def response_threads(name_db: str, queue_characters: int) -> None:
     """
     Функция обрабатывает запросы api: https://swapi.dev/api/. Передает их значения в БД.
     Args:
-        data_db: str: Данные для записи в БД.
+        name_db: str:  Название БД.
         queue_characters: int: Стартовая позиция очереди запросов к api.
 
     Returns:
         None
     """
-
-    while queue_characters:
-        logger.debug(f'Create request №{queue_characters}')
-        character: tuple[str, str, str] = star_wars_request(queue_characters)
-        if character:
-            data_db.append(character)
-            return
-        else:
-            queue_characters += 1
+    control_requests: int = 5
+    with db(name_db=name_db) as base:
+        while control_requests:
+            logger.debug(f'Create request №{queue_characters}')
+            character: tuple[str, str, str] = star_wars_request(queue_characters)
+            if character:
+                name, age, gender = character
+                base.insert_new_actor(name=name, age=age, gender=gender)
+                return
+            else:
+                control_requests -= 1
+                queue_characters += 1
 
 
 @timer
@@ -75,40 +67,41 @@ def major_thread(name_db: str) -> None:
         None
     """
     queue_characters: int = 1
-    completed = 0
-    data_requests: list[tuple] = []
-    while True:
-        logger.debug(f'Create request №{queue_characters}')
-        character: tuple[str, str, str] = star_wars_request(queue_characters)
+    completed: int = 0
 
-        if character:
-            data_requests.append(character)
-            completed += 1
-            queue_characters += 1
-        else:
-            queue_characters += 1
+    with db(name_db=name_db) as base:
+        while True:
+            logger.debug(f'Create request №{queue_characters}')
+            character: tuple[str, str, str] = star_wars_request(queue_characters)
 
-        if completed == 20:
-            sqlite3_db(data_requests, name_db=name_db)
-            break
+            if character:
+                name, age, gender = character
+                base.insert_new_actor(name=name, age=age, gender=gender)
+                completed += 1
+                queue_characters += 1
+            else:
+                queue_characters += 1
+
+            if completed == 20:
+                break
 
 
 @timer
-def twenty_threads(name_db: str) -> None:
+def ather_threads(name_db: str) -> None:
     """
-    Запускает 20 threads, которые в свою очередь обращаются к api: https://swapi.dev/api/.
+    Запускает 20 запросов в разных потоках, которые в свою очередь обращаются к api: https://swapi.dev/api/.
     Передает их значения в БД.
+    Args:
+        name_db: str: Название БД.
     Returns:
         None
     """
 
-    data_requests: list[tuple] = []
-    second_treads = [threading.Thread(target=response_threads,
-                                      kwargs={'data_db': data_requests, 'queue_characters': i}) for i in range(1, 21)]
+    other_treads = [threading.Thread(target=response_threads,
+                                     kwargs={'name_db': name_db, 'queue_characters': i}) for i in range(1, 21)]
 
-    for tread in second_treads:
+    for tread in other_treads:
         tread.start()
-    for tread in second_treads:
-        tread.join()
 
-    sqlite3_db(data_characters=data_requests, name_db=name_db)
+    for tread in other_treads:
+        tread.join()
