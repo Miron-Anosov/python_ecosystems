da471b6dc65e18e4b0957948a77e3c1c3d783749
Miron <mairon26rus@gmail.com>
2023-12-05 11:38:29 +0100

    renamed func from twenty_threads to ather_threads

--- module_11_multitasking/homework/hw2/main.py
+++ module_11_multitasking/homework/hw2/main.py
@@ -1,9 +1,9 @@
 import logging
 
-from sw_request_worker import major_thread as main_thread, twenty_threads
+from sw_request_worker import major_thread as main_thread, ather_threads
 
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
-    twenty_threads(name_db='sw_threads')
+    ather_threads(name_db='sw_threads')
     main_thread(name_db='sw_thread')
