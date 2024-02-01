a310ba7cb03f07717412bb32ac65899c29e9ef30
Miron Anosov <mairon26rus@gmail.com>
2024-02-01 18:11:10 +0200

    created new methods, added swagger_doc

--- module_18_documentation/homework/hw3/main.py
+++ module_18_documentation/homework/hw3/main.py
@@ -1,40 +1,37 @@
 import operator
+
 from flask import Flask
 from flask_jsonrpc import JSONRPC
+from flasgger import Swagger
+from flask_jsonrpc.exceptions import InvalidParamsError
 
 app = Flask(__name__)
 jsonrpc = JSONRPC(app, '/api', enable_web_browsable_api=True)
 
+swagger = Swagger(app, template_file='template/swagger.yml')
+
 
 @jsonrpc.method('calc.add')
-def add(a: float, b: float) -> float:
-    """
-    Пример запроса:
-
-    $ curl -i -X POST -H "Content-Type: application/json; indent=4" \
-        -d '{
-            "jsonrpc": "2.0",
-            "method": "calc.add",
-            "params": {"a": 7.8, "b": 5.3},
-            "id": "1"
-        }' http://localhost:5000/api
-
-    Пример ответа:
-
-    HTTP/1.1 200 OK
-    Server: Werkzeug/2.2.2 Python/3.10.6
-    Date: Fri, 09 Dec 2022 19:00:09 GMT
-    Content-Type: application/json
-    Content-Length: 54
-    Connection: close
-
-    {
-      "id": "1",
-      "jsonrpc": "2.0",
-      "result": 13.1
-    }
-    """
+def add(a: float | int, b: float | int) -> float | int:
     return operator.add(a, b)
+
+
+@jsonrpc.method('calc.sub')
+def minus(a: float | int, b: float | int) -> float | int:
+    return operator.sub(a, b)
+
+
+@jsonrpc.method('calc.mul')
+def mul(a: float | int, b: float | int) -> float | int:
+    return operator.mul(a, b)
+
+
+@jsonrpc.method('calc.div')
+def div(a: float | int, b: float | int) -> float | int:
+    try:
+        return operator.floordiv(a, b)
+    except Exception:
+        raise InvalidParamsError
 
 
 if __name__ == '__main__':
