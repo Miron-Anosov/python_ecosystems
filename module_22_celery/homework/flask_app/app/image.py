3a8d7232ecbfce7a07b1248f29339d8ffda4bf51
Miron Nikolaevich <mairon26rus@gmail.com>
2024-03-25 00:59:27 +0100

    добавлены пути к файлам изображений.

--- module_22_celery/homework/flask_app/app/image.py
+++ module_22_celery/homework/flask_app/app/image.py
@@ -1,10 +1,13 @@
 """
 Здесь происходит логика обработки изображения
 """
-
+import os
 from typing import Optional
 
 from PIL import Image, ImageFilter
+
+
+IMG_PATH = './img/'
 
 
 def blur_image(src_filename: str, dst_filename: Optional[str] = None):
@@ -12,10 +15,11 @@ def blur_image(src_filename: str, dst_filename: Optional[str] = None):
     Функция принимает на вход имя входного и выходного файлов.
     Применяет размытие по Гауссу со значением 5.
     """
+    abs_path_img = os.path.abspath(os.path.join('.', IMG_PATH))
+
     if not dst_filename:
         dst_filename = f'blur_{src_filename}'
-
-    with Image.open(src_filename) as img:
+    with Image.open(f'{abs_path_img}/{src_filename}') as img:
         img.load()
         new_img = img.filter(ImageFilter.GaussianBlur(5))
-        new_img.save(dst_filename)
+        new_img.save(f'{abs_path_img}/{dst_filename}')
