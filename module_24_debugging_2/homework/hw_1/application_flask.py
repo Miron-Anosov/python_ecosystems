97a6b69c98d201af0651133f2828176228f26419
Anosov Miron <mairon26rus@gmail.com>
2024-05-01 17:03:10 +0200

    create and start project

---
+++ module_24_debugging_2/homework/hw_1/application_flask.py
@@ -0,0 +1,26 @@
+import sentry_sdk
+from flask import Flask, jsonify
+
+sentry_sdk.init(
+    dsn="http://7ac684788d903d252611aa7717853c2d@127.0.0.1:9000/2",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+)
+
+app = Flask(__name__)
+
+
+@app.route(rule='/')
+@app.route(rule='/status', methods=["GET", ])
+def func() -> jsonify:
+    return jsonify({'ok': 200})
+
+
+@app.route(rule='/error')
+def err():
+    raise ZeroDivisionError('simple error')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
