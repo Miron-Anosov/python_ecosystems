cbd190fe663297cfd0c0017ac98e2ba9930571db
Miron <mairon26rus@gmail.com>
2024-05-31 18:12:37 +0200

    help

--- module_24_debugging_2/homework/hw_1/application_flask.py
+++ module_24_debugging_2/homework/hw_1/application_flask.py
@@ -2,10 +2,12 @@ import sentry_sdk
 from flask import Flask, jsonify
 
 sentry_sdk.init(
-    dsn="http://7ac684788d903d252611aa7717853c2d@127.0.0.1:9000/2",
+    dsn="http://3d75643a25637f6c7305e719a78fd07c@127.0.0.1:9000/2",
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
+    # traces_sample_rate=1.0,
+    # profiles_sample_rate=1.0,
+    # enable_tracing=True
 )
 
 app = Flask(__name__)
@@ -23,5 +25,4 @@ def err():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
- 
+    app.run(host='0.0.0.0')
