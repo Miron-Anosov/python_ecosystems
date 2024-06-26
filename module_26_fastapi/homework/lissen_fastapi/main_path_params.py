09eba4161776fb8c6192f7dfaa9eee92f18bd5f0
Miron <mairon26rus@gmail.com>
2024-06-26 20:42:37 +0200

    completed

--- module_26_fastapi/homework/lissen_fastapi/main_path_params.py
+++ module_26_fastapi/homework/lissen_fastapi/main_path_params.py
@@ -39,7 +39,7 @@ async def read_items(
 @app.get("/integer-items/{item_id}/{x}")
 async def read_items(
         item_id: Annotated[int, Path(ge=5, title="Items Int", description="greater-than-or-equal")],
-        x: Annotated[int, Path(le=5, ge=15, title="Less than or equal")]
+        x: Annotated[int, Path(le=15, ge=5, title="Less than or equal")]
 ):
     """
     Валидация числовых данных: больше и меньше или равно
@@ -49,6 +49,7 @@ async def read_items(
         gt: больше (greater than)
         le: меньше или равно (less than or equal)
         ge: должен быть больше или равен ("greater than or equal")
+        lt: меньше (less than)
     """
     results = {"item_id": item_id}
 
@@ -56,6 +57,34 @@ async def read_items(
         results.update({'x': x})
     return results
 
+
+@app.get("/float-items/{item_id}")
+async def read_items(
+        *,
+        item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+        q: str,
+        size: Annotated[float, Query(gt=0, lt=10.5)],
+):
+    """
+    Валидация числовых данных: числа с плавающей точкой, больше и меньше¶
+    Валидация также применима к значениям типа float.
+    В этом случае становится важной возможность добавить ограничение gt, вместо ge, поскольку в таком случае вы можете,
+    например, создать ограничение, чтобы значение было больше 0, даже если оно меньше 1.
+    Таким образом, 0.5 будет корректным значением. А 0.0 или 0 — нет.
+    То же самое справедливо и для lt.
+    """
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    print(size)
+    return results
+
+
+"""
+Query, Path и другие классы, которые мы разберём позже, являются наследниками общего класса Param.
+
+Все они используют те же параметры для дополнительной валидации и метаданных, которые вы видели ранее.
+"""
 
 
 " http://127.0.0.1:8000/openapi.json"
