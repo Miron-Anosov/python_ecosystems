d778c1ae9d2f1fc522c6ba6fa7183b0fb55aba06
Miron <mairon26rus@gmail.com>
2024-06-27 21:38:42 +0200

    docs(api): Created docs for api

--- module_26_fastapi/homework/lissen_fastapi/main_path_operation_config.py
+++ module_26_fastapi/homework/lissen_fastapi/main_path_operation_config.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Union
 
 from fastapi import FastAPI, status
@@ -14,6 +15,39 @@ class Item(BaseModel):
     tags: set[str] = set()
 
 
-@app.post("/items/", response_model=Item, status_code=status.HTTP_201_CREATED)
+class Tags(Enum):
+    items = "items"
+    users = "users"
+
+
+@app.post("/items/", response_model=Item, status_code=status.HTTP_201_CREATED,
+          summary="Create an item.",
+          description="Create an item with all the information, name, description, price, tax and a set of unique tags",
+          deprecated=True
+          )
 async def create_item(item: Item):
     return item
+
+
+@app.post("/items2/", response_model=Item, tags=[Tags.items], response_description="The created item..")
+async def create_item(item: Item):
+    """
+    Create an item with all the information:
+
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    """
+    return item
+
+
+@app.get("/items/", tags=[Tags.users])
+async def read_items():
+    return [{"name": "Foo", "price": 42}]
+
+
+@app.get("/users/", tags=["users"])
+async def read_users():
+    return [{"username": "johndoe"}]
