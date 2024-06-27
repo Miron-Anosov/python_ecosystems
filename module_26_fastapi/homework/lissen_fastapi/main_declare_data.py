7e13e45a236a9dde5e2b34beabc6fb1831869fcd
Miron <mairon26rus@gmail.com>
2024-06-27 02:01:56 +0200

    feat(update_item4): Add a new PUT route: /items4/{item_id}
     Help, swagger do not shows exaples schemas. I can not fix it

--- module_26_fastapi/homework/lissen_fastapi/main_declare_data.py
+++ module_26_fastapi/homework/lissen_fastapi/main_declare_data.py
@@ -28,6 +28,13 @@ class Item(BaseModel):
     }
 
 
+class ItemSimple(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
 class ItemTwo(BaseModel):
     """
     Дополнительные аргументы поля Field
@@ -64,21 +71,53 @@ async def update_item2(item_id: int, item: ItemTwo):
 
 @app.put("/items3/{item_id}")
 async def update_item(
-    item_id: int,
-    item: Annotated[
-        Item,
-        Body(
-            examples=[
-                {
-                    "name": "Foo",
-                    "description": "A very nice Item",
-                    "price": 35.4,
-                    "tax": 3.2,
-                }
-            ],
-        ),
-    ],
+        item_id: int,
+        item: Annotated[
+            Item,
+            Body(
+                examples=[
+                    {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    }
+                ],
+            ),
+        ],
 ):
     """Способ расширять документацию с помощью Annotated и Body. Удобно, если нельзя изменять объект."""
     results = {"item_id": item_id, "item": item}
+    return results
+
+
+@app.put("/items4/{item_id}")
+def update_item2(
+        *,
+        item_id: int,
+        item:
+        ItemSimple =
+        Body(
+            examples=[
+                {
+                    "name": "Foo2",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                },
+                {
+                    "name": "Bar",
+                    "price": "35.4",
+                },
+                {
+                    "name": "Baz",
+                    "price": "thirty five point four",
+                },
+            ],
+        )
+        ,
+):
+    results = {"item_id": item_id, "item": item}
+    """Данная схема дает возможность предоставить несколько вариантов для примера.
+     UI документация не поддерживает  как с Annotated так и без"""
     return results
