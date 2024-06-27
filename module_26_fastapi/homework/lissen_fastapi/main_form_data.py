be0aac25586eb2a5abe58286a94354d09a856e7a
Miron <mairon26rus@gmail.com>
2024-06-27 18:29:59 +0200

    dosc(main_form_data): Create some docs

--- module_26_fastapi/homework/lissen_fastapi/main_form_data.py
+++ module_26_fastapi/homework/lissen_fastapi/main_form_data.py
@@ -15,4 +15,15 @@ async def login(username: Annotated[str, Form()], password: Annotated[str, Form(
     Вы можете настроить Form точно так же, как настраиваете и Body ( Query, Path, Cookie), включая валидации,
     примеры, псевдонимы (например, user-name вместо username) и т.д.
     """
+    print(password)
     return {"username": username}
+
+
+"""
+О "полях формы"
+
+Обычно способ, которым HTML-формы (<form></form>) отправляют данные на сервер, использует "специальное" кодирование для 
+этих данных, отличное от JSON.
+
+FastAPI гарантирует правильное чтение этих данных из соответствующего места, а не из JSON.
+"""
