3538109d3a079c6f20097b0f1c983426f4acfd63
Miron <mairon26rus@gmail.com>
2024-06-27 13:47:27 +0200

    feat: Create new routes: /items3/ & /items4/

--- module_26_fastapi/homework/lissen_fastapi/main_response_model.py
+++ module_26_fastapi/homework/lissen_fastapi/main_response_model.py
@@ -3,6 +3,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from typing import Any
+
 app = FastAPI()
 
 
@@ -26,6 +28,7 @@ async def read_items() -> list[Item]:
         Item(name="Plumbus", price=32.0),
     ]
 
+
 """
 Валидации ответа.
 Если данные невалидны (например, отсутствует одно из полей), это означает, что код вашего приложения работает 
@@ -35,3 +38,22 @@ async def read_items() -> list[Item]:
 Но самое важное:
 Ответ будет ограничен и отфильтрован - т.е. в нем останутся только те данные, которые определены в возвращаемом типе.
 """
+
+
+@app.post("/items3/", response_model=Item)
+async def create_item(item: Item) -> Any:
+    return item
+
+
+@app.get("/items4/", response_model=list[Item])
+async def read_items() -> Any:
+    """
+    response_model принимает те же типы, которые можно указать для какого-либо поля в модели Pydantic.
+    Таким образом, это может быть как одиночная модель Pydantic, так и список (list) моделей Pydantic.
+    Например, List[Item].
+    """
+    return [
+        {"name": "Portal Gun", "price": 42.0},
+        {"name": "Plumbus", "price": 32.0},
+    ]
+
