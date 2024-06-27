34b8e2ab4ed3a506b81e351697704c69f7202e6b
Miron <mairon26rus@gmail.com>
2024-06-27 21:17:06 +0200

    feat(main_path_operation_config): Create Python module

--- module_26_fastapi/homework/lissen_fastapi/main_errors.py
+++ module_26_fastapi/homework/lissen_fastapi/main_errors.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
@@ -45,4 +48,36 @@ async def read_unicorn(name: str):
 Но оно будет обработано unicorn_exception_handler.
 Таким образом, вы получите чистую ошибку с кодом состояния HTTP 418 и содержимым JSON:
 {"message": "Oops! yolo did something. There goes a rainbow..."}
+"""
+
+#############################################################################################
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    print(exc.status_code)
+    print('StarletteHTTPException: обрабатывает HTTP-исключения, включая HTTPException')
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    print('RequestValidationError: обрабатывает ошибки валидации запроса.')
+    return PlainTextResponse(str(exc), status_code=400)
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    if item_id == 3:
+        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+    return {"item_id": item_id}
+
+
+"""
+Переопределение исключений проверки запроса
+Когда запрос содержит недопустимые данные, FastAPI внутренне вызывает ошибку RequestValidationError.
+А также включает в себя обработчик исключений по умолчанию.
+Чтобы переопределить его, импортируйте RequestValidationError и используйте его с 
+@app.exception_handler(RequestValidationError) для создания обработчика исключений.
+Обработчик исключения получит объект Request и исключение.
 """
