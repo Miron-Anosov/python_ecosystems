86a25f86a0a60f015e65da96e9e1c0d0d8bd1fa3
Miron <mairon26rus@gmail.com>
2024-06-28 00:13:37 +0200

    feat(read_query): Created a new GET routes /items-fist-depends/

--- module_26_fastapi/homework/lissen_fastapi/main_dependency_injection.py
+++ module_26_fastapi/homework/lissen_fastapi/main_dependency_injection.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Cookie
 
 app = FastAPI()
 
@@ -45,3 +45,28 @@ async def read_items(commons: Annotated[CommonQueryParams, Depends(CommonQueryPa
     items = fake_items_db[commons.skip: commons.skip + commons.limit]
     response.update({"items": items})
     return response
+
+
+###################################################################################
+
+def query_extractor(q: str | None = None):
+    return q
+
+
+def query_or_cookie_extractor(q: Annotated[str, Depends(query_extractor)],
+                              last_query: Annotated[str | None, Cookie()] = None,
+                              ):
+    """Используем зависимость для того что бы отдать новые данные или из cookie"""
+    if not q:
+        return last_query
+    return q
+
+
+@app.get("/items-fist-depends/")
+async def read_query(
+        query_or_default: Annotated[str, Depends(query_or_cookie_extractor)],
+):
+    return {"q_or_cookie": query_or_default}
+
+######################################################################################
+
