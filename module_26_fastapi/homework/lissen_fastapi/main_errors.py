68f5b07954acb9a984f75cbbea94dc6c6dca4b45
Miron <mairon26rus@gmail.com>
2024-06-27 20:41:14 +0200

    feat(read_unicorn): Create GET route /unicorns/{name}

--- module_26_fastapi/homework/lissen_fastapi/main_errors.py
+++ module_26_fastapi/homework/lissen_fastapi/main_errors.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -14,3 +15,34 @@ async def read_item_header(item_id: str):
             headers={"X-Error": "There goes my error"},
         )
     return {"item": items[item_id]}
+
+
+###################################################################
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+    )
+
+
+@app.get("/unicorns/{name}")
+async def read_unicorn(name: str):
+    if name == "yolo":
+        raise UnicornException(name=name)
+    return {"unicorn_name": name}
+
+
+"""
+Здесь, если запросить /unicorns/yolo, то операция пути вызовет UnicornException.
+Но оно будет обработано unicorn_exception_handler.
+Таким образом, вы получите чистую ошибку с кодом состояния HTTP 418 и содержимым JSON:
+{"message": "Oops! yolo did something. There goes a rainbow..."}
+"""
