a60850b49733a77410a803af7cd49e0cb8c364b0
Miron <mairon26rus@gmail.com>
2024-07-02 16:35:10 +0200

    dosc(test_get_all_recipes): Create some docs for test

--- module_26_fastapi/homework/backend/tests/test_api_routes.py
+++ module_26_fastapi/homework/backend/tests/test_api_routes.py
@@ -9,7 +9,7 @@ GET_ALL_RECIPES_URI_PREFIX = '/recipes'
 
 @pytest.mark.anyio
 async def test_get_all_recipes():
-
+    """Successful test route: /recipes  code == 200"""
     async with AsyncClient(app=app, base_url=BASE_URL) as test_route:
         response = await test_route.get(GET_ALL_RECIPES_URI_PREFIX)
         assert response.status_code == codes.OK
