ddc1a3c30820301a0221bba89ad2301e373f78f6
Miron <mairon26rus@gmail.com>
2024-07-08 19:45:11 +0200

    fix(del): Deleted function wich dont use

--- module_26_fastapi/homework/test/test_api_routes.py
+++ module_26_fastapi/homework/test/test_api_routes.py
@@ -3,71 +3,25 @@ from httpx import codes
 import httpx
 import pytest_asyncio
 
-
 from asgi_lifespan import LifespanManager
 
-from ..api.core.orm_core.models.base import BaseORM
-from ..api.core.settings import setting
-from ..api.core.orm_core.engine import AsyncCoreDB
 
-# @pytest.fixture(scope="session")
-# def event_loop():
-#     loop = asyncio.get_event_loop_policy().new_event_loop()
-#     yield loop
-#     loop.close()
-
-
-DATABASE_URL = setting.test.host_database_sqlalchemy
-db = AsyncCoreDB(url=DATABASE_URL, echo=True)
+from ..api.core.settings import settings
 
 try:
-    assert setting.test.MODE == 'TEST', 'Research our env'
+    assert settings.MODE == 'TEST', f'Invalid mode: {settings.MODE}, miss TEST config.'
 except AssertionError:
     exit(0)
 
 
-@pytest_asyncio.fixture(scope="session")
-async def engine():
-    engine = db.async_engine
-    yield engine
-    await db.async_engine.dispose()
-
-
-@pytest_asyncio.fixture(scope="session", autouse=True)
-async def database(engine):
-    async with engine.begin() as conn:
-        await conn.run_sync(BaseORM.metadata.create_all)
-    yield
-    async with engine.begin() as conn:
-        await conn.run_sync(BaseORM.metadata.drop_all)
-
-
-@pytest_asyncio.fixture(scope="function")
-async def async_session(engine):
-    async_scoped_session_maker = db.async_scoped_session()
-    session = async_scoped_session_maker()
-    try:
-        yield session
-    finally:
-        await session.close()
-        await async_scoped_session_maker.remove()
-
-
 @pytest_asyncio.fixture
-async def app():
+async def client():
     from ..api.main import app
     async with LifespanManager(app) as manager:
-        yield manager.app
-
-
-@pytest_asyncio.fixture
-async def client(app):
-    async with LifespanManager(app) as manager:
-        async with httpx.AsyncClient(transport=httpx.ASGITransport(app=app)) as client:
+        async with httpx.AsyncClient(transport=httpx.ASGITransport(app=manager.app)) as client:
             yield client
 
 
-@pytest.mark.usefixtures("client")
 class BaseTest:
     BASE_URL = "http://testserver"
     RECIPES_PREFIX = '/v1/recipes'
@@ -89,6 +43,4 @@ class TestGetRecipesAll(BaseTest):
         """GET /v1/recipes all recipes"""
         url = f"{self.BASE_URL}{self.RECIPES_PREFIX}/"
         response = await client.get(url=url)
-        print(response)
-        print(response.url)
         assert response.status_code == codes.OK
