c41d7c3fcfd50d2c752823d4598ace1513896e87
Miron <mairon26rus@gmail.com>
2024-07-08 19:42:21 +0200

    refactor(engine): Create function
    Make engine on with creator_engine

--- module_26_fastapi/homework/api/core/orm_core/__init__.py
+++ module_26_fastapi/homework/api/core/orm_core/__init__.py
@@ -1,8 +1,13 @@
 from .crud import crud
 from .engine import AsyncCoreDB
-from ..settings import setting
+from ..settings import settings
 from .models.base import BaseORM
 
-engine = AsyncCoreDB(url=setting.test.host_database_sqlalchemy, echo=setting.prod.ECHO)
+
+def create_engine(url: str = settings.host_database_sqlalchemy, echo: bool = settings.ECHO) -> AsyncCoreDB:
+    return AsyncCoreDB(url=url, echo=echo)
+
+
+engine = create_engine()
 
 __all__ = ['engine', 'crud', 'BaseORM']
