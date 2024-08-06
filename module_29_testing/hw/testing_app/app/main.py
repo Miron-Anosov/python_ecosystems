789a256a8391fac672d26b6d06c1b2cd7a8ade65
Miron <mairon26rus@gmail.com>
2024-08-06 19:17:06 +0200

    refactor: Я не знаю почему, но под WSL ubuntu таблицы не создавались

--- module_29_testing/hw/testing_app/app/main.py
+++ module_29_testing/hw/testing_app/app/main.py
@@ -9,27 +9,23 @@ from orm_models import db
 def make_app(db_host_url: Optional[str] = None, testing: bool = False,
              debug: bool = False) -> Flask:
 
-    app: Flask = Flask(__name__)
-    app.config["TESTING"] = testing
-    app.config["DEBUG"] = debug
+    _app: Flask = Flask(__name__)
+    _app.config["TESTING"] = testing
+    _app.config["DEBUG"] = debug
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_host_url \
+    _app.config["SQLALCHEMY_DATABASE_URI"] = db_host_url \
         if db_host_url else "sqlite:///project.db"
 
-    app.register_blueprint(parking)
+    _app.register_blueprint(parking)
 
-    db.init_app(app)
+    db.init_app(_app)
 
-    # with app.app_context():
-    #     db.create_all()
+    with _app.app_context():
+        db.create_all()
 
-    return app
+    return _app
 
 
 if __name__ == "__main__":
     app = make_app(debug=True)
-    with app.app_context():
-        db.create_all()
-        db.session.commit()
-
     app.run(host="0.0.0.0", port=8080)
