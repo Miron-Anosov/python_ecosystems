3eb8b2dca4200008fa8061cfb5fa637c2a37da34
Miron <mairon26rus@gmail.com>
2024-08-09 18:27:16 +0200

    feat(fixture): add fixtures

--- module_29_testing/hw/testing_app/tests/conftest.py
+++ module_29_testing/hw/testing_app/tests/conftest.py
@@ -3,23 +3,42 @@ import pytest
 from typing import Generator
 
 from flask import Flask
+from flask.testing import FlaskClient, FlaskCliRunner
 
-from app.main import make_app
+from app.main import make_app  # noqa
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def flask_app() -> Generator[Flask, None, None]:
-    app = make_app(testint=True, host='127.0.0.1', port=5000)
-    
+    host_db = "sqlite:///test.db"
+    app: Flask = make_app(db_host_url=host_db, testing=True, debug=True)
+
     yield app
-    app.teardown_appcontext()
+
+    with app.app_context():
+        app.config["db"].drop_all()
 
 
-@pytest.fixture()
-def client(flask_app: Flask) -> Flask:
-    return flask_app.test_client()
+@pytest.fixture(scope="session")
+def client(flask_app: Flask) -> FlaskClient:
+    yield flask_app.test_client()
 
 
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
+@pytest.fixture(scope="function")
+def client_scope_only_func(flask_app: Flask) -> FlaskClient:
+    yield flask_app.test_client()
+
+
+@pytest.fixture(scope="session")
+def runner(flask_app) -> FlaskCliRunner:
+    yield flask_app.test_cli_runner()
+
+
+@pytest.fixture(scope="module")
+def get_all_client_response(client: FlaskClient):
+    yield client.get("/clients")
+
+
+@pytest.fixture(scope="module")
+def get_client_by_id_response(client: FlaskClient):
+    yield client.get("/clients/1")
