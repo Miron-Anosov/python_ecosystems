44a6318bbfd5f164119f5833fd5d98b59f7b1818
Miron <mairon26rus@gmail.com>
2024-08-09 18:30:14 +0200

    fix(config): add db obj to app.config

--- module_29_testing/hw/testing_app/app/main.py
+++ module_29_testing/hw/testing_app/app/main.py
@@ -6,26 +6,25 @@ from controllers import parking
 from orm_models import db
 
 
-def make_app(db_host_url: Optional[str] = None, testing: bool = False,
-             debug: bool = False) -> Flask:
+def make_app(
+    db_host_url: Optional[str] = None, testing: bool = False, debug: bool = False
+) -> Flask:
 
     _app: Flask = Flask(__name__)
     _app.config["TESTING"] = testing
     _app.config["DEBUG"] = debug
 
-    _app.config["SQLALCHEMY_DATABASE_URI"] = db_host_url \
-        if db_host_url else "sqlite:///project.db"
+    _app.config["SQLALCHEMY_DATABASE_URI"] = (
+        db_host_url if db_host_url else "sqlite:///project.db"
+    )
 
     _app.register_blueprint(parking)
 
     db.init_app(_app)
 
+    _app.config["db"] = db
+
     with _app.app_context():
         db.create_all()
 
     return _app
-
-
-if __name__ == "__main__":
-    app = make_app(debug=True)
-    app.run(host="0.0.0.0", port=8080)
